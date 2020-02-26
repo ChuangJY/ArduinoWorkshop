3e31a1e5b240a37fd3a634446bb048550e0aef92
SebastianJY <jinyee97@gmail.com>
2020-02-26 19:57:46 +0800

    First Commit

--- Sys/Sys.ino
+++ Sys/Sys.ino
@@ -5,8 +5,6 @@
 // Pin Rx1 18, Tx1 19
 // ****LCD Module****
 // Pin D4,D5,D6,D7,D8,D9
-// ****PIR Sensor Module****
-// Pin D11
 // ****IR Sensor Module****
 // Pin D2
 // ****Servo Motor****
@@ -26,7 +24,6 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 //Global constant
 #define BUZZER 10
-#define PIR 11
 #define IR 2
 #define SERVO 3
 #define BLESTATUSPIN 13
@@ -44,7 +41,9 @@ boolean timer1Flag = false;
 boolean timer2Flag = false;
 int current = 0;
 int count = 0;
-int rotationStartTime;
+int rotatedTime = 0;
+long rotationStartTime;
+long lastRotateTime = 0;
 struct feedTime {
   short int feedHour;
   short int feedMinute;
@@ -96,9 +95,6 @@ void setup() {
   //Initialize IR sensor
   pinMode(IR, INPUT);
 
-  //Initialize PIR Sensor
-  pinMode(PIR, INPUT);
-
   //Getting default feed time from EEPROM
   int addr = 0;
   char data[8];
@@ -116,7 +112,7 @@ void setup() {
 
 void loop() {
 
-  DateTime now = rtc.now();
+  DateTime noww = rtc.now();
 
   //Store food level status
   if (digitalRead(IR) == LOW) {
@@ -135,11 +131,20 @@ void loop() {
     //Read time setting data from our phone and store into EEPROM
     if (Serial1.available()) {
       Serial1.readBytes(buff, sizeof(buff));
+      delay(10);
       Serial.println(sizeof(buff));
       int i = 0;
+      Serial.println("Verify data before writing");
+      int hourr;
+      int minn;
+      while (i < 8) {
+        //Depending on how you want to check the buffer
+        //let's say if hour > 24 or hour < 0 or minute > 60 or minute < 0
+
+      }
       Serial.println("Writing feed time into EEPROM: ");
       while (i < 8) {
-        Serial.print(buff[i]);
+        //Serial.print(buff[i]);
         EEPROM.write(i, buff[i]);
         i++;
       }
@@ -150,64 +155,19 @@ void loop() {
   }
 
   //Move the servo when feed time is up
-  if (sizeof(buff) != 8) {
-    //Received wrong package, discard
-    Serial.println("Wrong message");
-    int i = 0;
-    while (i < 8) {
-      buff[i++] = 0;
-    }
-  } else {
-    if (feedTime1.feedHour == 16 && feedTime1.feedMinute == 37  && now.second() == 50) {
+  if ((feedTime1.feedHour == noww.hour() && feedTime1.feedMinute == noww.minute()) || (feedTime2.feedHour == noww.hour() && feedTime2.feedMinute == noww.minute())) {
+    if (noww.second() == 0) {
+      myservo.write(OPENPOS);
       rotationStartTime = millis();
-      while (millis() - rotationStartTime < 3000) {
-        myservo.write(OPENPOS);
-        displayTimeAndBLEStatus();
-        //Keep buzzer ringging
+      if (rotatedTime = 0) {
+        lastRotateTime = millis();
+        rotatedTime = 1;
       }
-      while (millis() - rotationStartTime < 5000) {
+      if ((rotationStartTime - lastRotateTime) >= 3000 && rotatedTime == 1) {
         myservo.write(CLOSEPOS);
-        displayTimeAndBLEStatus();
-        //Stop buzzer from ringging
-      }
-    }
-
-    if (feedTime2.feedHour == now.hour() && feedTime2.feedMinute == now.minute() && now.second() == 0) {
-      rotationStartTime = now.second();
-      while (now.second() - rotationStartTime < 4) {
-        myservo.write(OPENPOS);
-        displayTimeAndBLEStatus();
-        //Keep buzzer ringging
-      }
-      while (now.second() - rotationStartTime < 6) {
-        myservo.write(CLOSEPOS);
-        displayTimeAndBLEStatus();
-        //Stop buzzer from ringging
+        rotatedTime = 0;
       }
     }
   }
-
   displayTimeAndBLEStatus();
-}
-
-void displayTimeAndBLEStatus() {
-  DateTime now = rtc.now();
-  lcd.setCursor(0, 0);
-  lcd.print(now.year() , DEC);
-  lcd.print('/');
-  lcd.print(now.month(), DEC);
-  lcd.print('/');
-  lcd.print(now.day(), DEC);
-  lcd.print(' ');
-  lcd.print(Week[now.dayOfTheWeek()]);
-  lcd.setCursor(0, 1);
-  lcd.print(now.hour(), DEC);
-  lcd.print(':');
-  lcd.print(now.minute(), DEC);
-  lcd.print(':');
-  lcd.print(now.second(), DEC);
-  lcd.print(' ');
-  BTConnected ? lcd.write('Y') : lcd.write('N');
-  lcd.print('/');
-  foodLevel ? lcd.write('F') : lcd.write('E');
 }
